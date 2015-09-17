13074e9ccc4086346dee7b55ce4c06a5f2dad34e
Blue Silver <bluesilver0702@hotmail.com>
2015-09-18 01:49:26 +0800

    v1.0.1

---
+++ src/com/cantecegradinita/ro/PlayerActivity.java
@@ -0,0 +1,13 @@
+package com.cantecegradinita.ro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
