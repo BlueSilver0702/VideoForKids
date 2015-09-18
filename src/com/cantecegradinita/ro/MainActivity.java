7fbd4056219b70f9ea6b17d6795486eb898c9854
Blue Silver <bluesilver0702@hotmail.com>
2015-09-19 00:50:48 +0800

    v1-2-2

--- src/com/cantecegradinita/ro/MainActivity.java
+++ src/com/cantecegradinita/ro/MainActivity.java
@@ -2,6 +2,9 @@ package com.cantecegradinita.ro;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btn_setting = (Button)findViewById(R.id.Button03);
+        Button btn_setting_back = (Button)findViewById(R.id.btn_setting_back);
+        
+        btn_setting.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				System.out.println(">>>>>>>>>>>>>>>>>> : ahahah");
+			}
+		});
+        
+        btn_setting_back.setOnClickListener(new OnClickListener() {
+        	@Override
+			public void onClick(View v) {
+				System.out.println(">>>>>>>>>>>>>>>>>> : ahahah");
+			}
+        });
     }
 }
